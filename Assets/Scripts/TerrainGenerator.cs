61c97ad041ac8df0d84606c14bcdbd1b5d81dd72
TheBricktop <TheBricktop@users.noreply.github.com>
2020-06-02 19:45:04 +0200

    Djenga

---
+++ Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    public GameObject terrainChunk;
+
+    public Vector2 mapSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+              var newGroundObject = Instantiate(terrainChunk, new Vector3(x, 0, y), Quaternion.Euler(90,0,0));
+                
+            }
+
+        }
+
+
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   
+}
---
+++ Assets/lamb.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lamb : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float jumpForce = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //InvokeRepeating("Jump",1,1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+    public void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+}
