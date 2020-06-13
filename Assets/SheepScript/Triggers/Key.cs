ee02eed51167fc885205e34a348c3b55b8c39a53
TheBricktop <TheBricktop@users.noreply.github.com>
2020-06-13 16:32:17 +0200

    SheepScripts stump
    creating the project structure

--- Assets/SheepScript/Triggers/Key.cs
+++ Assets/SheepScript/Triggers/Key.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Key : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+namespace SheepScript
+{
+    /// <summary>
+    /// Simple input test for keyboard
+    /// </summary>
+    public class Key : Stimulus
     {
-        
-    }
+        public KeyCode inputKey;
+        private void Update()
+        {
+            if (Input.GetKeyDown(inputKey))
+            {
+                CurrentState = stimulusState.start;
+            }
+
+            if (Input.GetKey(inputKey))
+            {
+                CurrentState = stimulusState.stay;
+            }
+
+            if (Input.GetKeyUp(inputKey))
+            {
+                CurrentState = stimulusState.end;
+            }
+        }
+    }  
 }
+
